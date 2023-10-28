5a9477184f5d4c74de7e787ff52afb3e7a4a9be9
vsmm-world <thekingjarvis792@gmail.com>
2023-10-28 21:21:23 +0530

    some script changes

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const port = 3000;
 const path = require('path');
 const { title } = require('process');
 
-
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
@@ -24,15 +23,6 @@ app.listen(port,()=>{
     console.log("Website is Running on : "+`http://localhost:${port}`);
 })
 
-const newSchema= new mongoose.Schema({
-
-    name:String,
-    age:Number
-
-});
-
-const Mod = mongoose.model('mod',newSchema);
-
 const TodoSchema = new mongoose.Schema({
 
     title:String,
@@ -41,10 +31,6 @@ const TodoSchema = new mongoose.Schema({
 
 const Todo = mongoose.model('Todo',TodoSchema);
 
-const del = async ()=>{
-    const dl= await Mod.deleteMany({});
-    console.log(dl);
-}
 
 app.post('/api/todos',(req,res)=>{
 
